4ed2f6ed49b41e53708f07d3b5b8a397ba9d6fd1
DESKTOP-3J1TPVV\Nicolaev <ionnicolaevv@yahoo.com>
2018-10-30 10:38:17 +0200

    Class Test change

--- TestProjectRepository/src/ro/java/Test.java
+++ TestProjectRepository/src/ro/java/Test.java
@@ -3,4 +3,26 @@ package ro.java;
 public class Test {
 	private String da;
 	private String nu;
+
+	public String getDa() {
+		return da;
+	}
+
+	public void setDa(String da) {
+		this.da = da;
+	}
+
+	public String getNu() {
+		return nu;
+	}
+
+	public void setNu(String nu) {
+		this.nu = nu;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
 }
